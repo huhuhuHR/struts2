3c0916e7be2527cfe30b9a881939ceee2f7b17d5
huorong <757033301@qq.com>
2017-07-01 15:55:22 +0800

    idear struts2

---
+++ src/main/java/com/hh/controller/TestAction.java
@@ -0,0 +1,11 @@
+package com.hh.controller;
+/**
+ * Created by huorong on 17/7/1.
+ */
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+    public String testOne() {
+        return "index";
+    }
+}
