a2031aeef8236fe48ab999f151729e8da493cf2d
huhuhuHR <757033301@qq.com>
2017-07-02 08:33:56 +0800

    整合spring－batis；spring管理对象

--- src/main/java/com/hh/controller/TestAction.java
+++ src/main/java/com/hh/controller/TestAction.java
@@ -1,11 +1,20 @@
 package com.hh.controller;
+
 /**
  * Created by huorong on 17/7/1.
  */
+
+import com.hh.service.TestService;
 import com.opensymphony.xwork2.ActionSupport;
 
+import javax.annotation.Resource;
+
 public class TestAction extends ActionSupport {
+    @Resource
+    TestService testService;
+
     public String testOne() {
+        String result = testService.getUser();
         return "index";
     }
 }
